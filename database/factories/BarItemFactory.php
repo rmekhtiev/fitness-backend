61d5c6d9f14702364e424b4337c78d55ed721d15
Leonid Meleshin <leon0399@explabs.ru>
2019-10-09 15:33:05 +0300

    feat(Bar): proper faker names

--- database/factories/BarItemFactory.php
+++ database/factories/BarItemFactory.php
@@ -2,13 +2,16 @@
 
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 
-use App\Model;
+use App\FakerRestaurant\Restaurant\ru_RU\Restaurant;
 use App\Models\BarItem;
 use Faker\Generator as Faker;
 
+
 $factory->define(BarItem::class, function (Faker $faker) {
+    $faker->addProvider(new Restaurant($faker));
+
     return [
-        'title' => $faker->company,
+        'title' => $faker->foodName(),
         'amount' => $faker->numberBetween($min=1, $max=100),
     ];
 });
