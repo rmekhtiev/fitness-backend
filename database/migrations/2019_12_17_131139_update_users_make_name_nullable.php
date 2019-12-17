b7d3507dc5a6352feafaec539b34a390095dcddf
rusiano <rs.mekhtiev@gmail.com>
2019-12-17 16:20:59 +0300

    feat(users):  update validation, makes name field nullable

---
+++ database/migrations/2019_12_17_131139_update_users_make_name_nullable.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class UpdateUsersMakeNameNullable extends Migration
+{
+    const TABLE_NAME = 'users';
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create(static::TABLE_NAME, function (Blueprint $table) {
+            $table->string('name')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table(static::TABLE_NAME, function (Blueprint $table) {
+            $table->string('name');
+        });
+    }
+}
